441af8e9543abfde9bff7c659235c1cafe2c8df3
Danton Ivanochko <dantonivanochko@gmail.com>
2023-11-15 10:25:44 -0500

    Update protein_modeling_functions_pymol.py

--- protein_modeling_functions_pymol.py
+++ protein_modeling_functions_pymol.py
@@ -2,6 +2,9 @@
 
 
 # pymol
+import pymol
+pymol.pymol_argv = ['pymol','-qc']
+pymol.finish_launching()
 from pymol import cmd, stored, util
 # misc
 import os
@@ -120,7 +123,40 @@ def RMSD(query_structure, reference_structure):
         return rmsd
 cmd.extend("RMSD", RMSD)
 
+def count_interchain_clashes(chain1, chain2, clash_cutoff=0.4):
 
+        """
+        Compute the number of bad clashes between two selections.
+        Bad clashes (MolProbity) occur when the van der Waals radii overlap by ≥ 0.4 Å.
+        """
+
+        object_sele1 = pymol.cmd.get_names(selection=chain1)[0]
+        object_sele2 = pymol.cmd.get_names(selection=chain2)[0]
+
+        atom_ids_sele1 = pymol.cmd.identify(chain1)
+        atom_ids_sele2 = pymol.cmd.identify(chain2)
+
+        atom_ids_sele1 = "+".join(str(x) for x in atom_ids_sele1)
+        atom_ids_sele2 = "+".join(str(x) for x in atom_ids_sele2)
+
+        pymol.stored.list_sele1 = []
+        pymol.stored.list_sele2 = []
+
+        pymol.cmd.iterate( f"({object_sele1} and ID {atom_ids_sele1})", "stored.list_sele1.append([model, segi, chain, resn, resi, ID, name, elem, vdw])" )
+        pymol.cmd.iterate( f"({object_sele2} and ID {atom_ids_sele2})", "stored.list_sele2.append([model, segi, chain, resn, resi, ID, name, elem, vdw])" )
+
+        clash_count = 0
+
+        for i in stored.list_sele1:
+            for j in stored.list_sele2:
+                clashing_distance = i[8] + j[8] - 0.4 
+                measured_distance = pymol.cmd.get_distance(f"(/{i[0]}/{i[1]}/{i[2]}/{i[4]}/{i[6]})",f"(/{j[0]}/{j[1]}/{j[2]}/{j[4]}/{j[6]})")
+                if measured_distance < clashing_distance:
+                    clash_count += 1
+
+        return clash_count
+
+pymol.cmd.extend("count_interchain_clashes", count_interchain_clashes)
 
 
 def get_aa_seq(pymol_object): # only one chain at a time
