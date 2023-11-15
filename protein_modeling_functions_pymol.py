853455cd63debb72e8d31e428208c15c84282366
Danton Ivanochko <dantonivanochko@gmail.com>
2023-11-15 14:57:42 -0500

    Update protein_modeling_functions_pymol.py

--- protein_modeling_functions_pymol.py
+++ protein_modeling_functions_pymol.py
@@ -126,7 +126,8 @@ cmd.extend("RMSD", RMSD)
 
 def count_interchain_clashes(chain1, chain2, clash_cutoff=0.4):
         """
-        Compute the number of bad clashes between two selections.
+        Compute the number of (bad) clashes between two selections.
+	Clashes occur if vander Waals radii come into contact.
         Bad clashes (MolProbity) occur when the van der Waals radii overlap by ≥ 0.4 Å.
         """
         object_sele1 = pymol.cmd.get_names(selection=chain1)[0]
@@ -134,20 +135,24 @@ def count_interchain_clashes(chain1, chain2, clash_cutoff=0.4):
         atom_ids_sele1 = pymol.cmd.identify(f"{chain1} within 4 of {chain2}")
         atom_ids_sele2 = pymol.cmd.identify(f"{chain2} within 4 of {chain1}")
         atom_ids_sele1 = "+".join(str(x) for x in atom_ids_sele1)
-        atom_ids_sele2 = "+".join(str(x) for x in atom_ids_sele2)
-        pymol.stored.list_sele1 = []
-        pymol.stored.list_sele2 = []
-        pymol.cmd.iterate( f"({object_sele1} and ID {atom_ids_sele1})", "stored.list_sele1.append([model, segi, chain, resn, resi, ID, name, elem, vdw])" )
-        pymol.cmd.iterate( f"({object_sele2} and ID {atom_ids_sele2})", "stored.list_sele2.append([model, segi, chain, resn, resi, ID, name, elem, vdw])" )
+        atom_ids_sele2 = "+".join(str(x) for x in atom_ids_sele2)       
         clash_count = 0
-        for i in stored.list_sele1:
-                for j in stored.list_sele2:
-                        clashing_distance = i[8] + j[8] - float(clash_cutoff)
-                        measured_distance = pymol.cmd.get_distance(f"(/{i[0]}/{i[1]}/{i[2]}/{i[4]}/{i[6]})",f"(/{j[0]}/{j[1]}/{j[2]}/{j[4]}/{j[6]})")
-                        if measured_distance < clashing_distance:
-                                clash_count += 1
-        print(clash_count)
-        return clash_count
+        if not atom_ids_sele1 or not atom_ids_sele2:
+                print(clash_count)
+                return clash_count
+        else:
+                pymol.stored.list_sele1 = []
+                pymol.stored.list_sele2 = []
+                pymol.cmd.iterate( f"({object_sele1} and ID {atom_ids_sele1})", "stored.list_sele1.append([model, segi, chain, resn, resi, ID, name, elem, vdw])" )
+                pymol.cmd.iterate( f"({object_sele2} and ID {atom_ids_sele2})", "stored.list_sele2.append([model, segi, chain, resn, resi, ID, name, elem, vdw])" )
+                for i in stored.list_sele1:
+                        for j in stored.list_sele2:
+                                clashing_distance = i[8] + j[8] - float(clash_cutoff)
+                                measured_distance = pymol.cmd.get_distance(f"(/{i[0]}/{i[1]}/{i[2]}/{i[4]}/{i[6]})",f"(/{j[0]}/{j[1]}/{j[2]}/{j[4]}/{j[6]})")
+                                if measured_distance < clashing_distance:
+                                        clash_count += 1
+                print(clash_count)
+                return clash_count
 pymol.cmd.extend("count_interchain_clashes", count_interchain_clashes)
 
 
